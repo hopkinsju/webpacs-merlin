7adb8317b9a82e8002581a82cfa218f66217040f
hardy <hardy@243b6f06-4adc-0310-b70d-e36f471213ad>
2007-12-07 06:28:14 +0000

    generated first file via the template toolkit; added more infrastructure;
    git-svn-id: https://svn.test.lso.umsystem.edu/misc/merlin_webpac/trunk@1515 243b6f06-4adc-0310-b70d-e36f471213ad

--- bin/configure.pl
+++ bin/configure.pl
@@ -1,16 +1,16 @@
 #!/usr/local/bin/perl -w
 #
 # configure
-# 
-# This script determines the correct root directory 
-# for the project (the parent of the 'bin' directory 
-# in which it is located), prompts for some configuration 
+#
+# This script determines the correct root directory
+# for the project (the parent of the 'bin' directory
+# in which it is located), prompts for some configuration
 # values if not set via command-line options, and then
 # calls ttree to process all files under the skeleton
 # directory, storing output relative to the project root
 # directory (e.g., skeleton/bin/build => bin/build).
 #
-# Copyright 2003 Andy Wardley.  
+# Copyright 2003 Andy Wardley.
 #
 # This is free software distributed under the same terms as Perl.
 #
@@ -23,10 +23,11 @@ local $|=1;
 
 # defaults
 my $URL  = '/screens';
+my $INI  = 'anonymous';
 
 # get options
-our ($opt_d, $opt_u, $opt_y, $opt_h);
-getopts('yhdu:');
+our ($opt_d, $opt_u, $opt_y, $opt_h, $opt_i);
+getopts('yhdui:');
 
 # display usage and exit on -h
 die <<END_USAGE if $opt_h;
@@ -36,11 +37,12 @@ options:
     -u url    url for HTML pages  (default: $URL)
     -d debug  set debug flag      (default: 0)
     -y        Accept defaults
+    -i        your initials
     -h        This help
 END_USAGE
 
 # work out where we are in the filesystem
-my @dirs = File::Spec->splitdir($Bin); 
+my @dirs = File::Spec->splitdir($Bin);
 pop @dirs;  # remove 'bin'
 my $dir  = File::Spec->catdir(@dirs);
 my $skel = File::Spec->catfile($dir, 'skeleton');
@@ -50,7 +52,8 @@ my $cfg  = File::Spec->catfile($dir, 'etc/configure.cfg');
 
 # prompt for root URL
 my $url  = prompt('root page URL', $opt_u || $URL);
-my $dbg  = prompt('enable debugging?', $opt_d ? 'yes' : 'no') 
+my $initials  = prompt('your initials', $opt_i || $INI);
+my $dbg  = prompt('enable debugging?', $opt_d ? 'yes' : 'no')
          =~ /^y(es)?/ ? 1 : 0;
 
 $dir  = qq("$dir");
@@ -66,6 +69,7 @@ my @args = ( 'ttree',
              '--define', "dir=$dir",
              '--define', "url=$url",
              '--define', "debug=$dbg",
+             '--define', "initials=$initials",
              @ARGV );
 
 print "@args\n";
